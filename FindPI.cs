d2b2cf28c4f8c053004c2bccc84e01336168c685
FizzyCantCode <45712826+FizzyRust@users.noreply.github.com>
2024-07-16 17:02:59 +0300

    pies

---
+++ FindPI.cs
@@ -0,0 +1,13 @@
+﻿int piDecimalPlaces = -1;
+do
+{
+    Console.Clear();
+    Console.WriteLine("How many decimal places of PI do you desire? (range of 0 to 15)");
+    string userinput = Console.ReadLine();
+    if (!int.TryParse(userinput, out piDecimalPlaces))
+    {
+        piDecimalPlaces = -1;
+    }
+} while (piDecimalPlaces > 15 || piDecimalPlaces < 0);
+
+Console.WriteLine("This is your PI: " + Math.Round(Math.PI, piDecimalPlaces));
